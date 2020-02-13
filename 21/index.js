dc83a0a8175ea7ba00393da2a23b819284772856
CCliff <clcliff30@gmail.com>
2020-02-13 17:15:57 -0500

    21 Solved

---
+++ 21/index.js
@@ -0,0 +1,21 @@
+(() => {
+  let sol = 0;
+  const map = {};
+
+  for (let i = 2; i < 10000; i++) {
+    let sum = 0;
+    for (let j = 1; j <= i / 2; j++) {
+      if (i % j === 0) {
+        sum += j;
+      }
+    }
+    map[i] = sum;
+
+    if (sum !== i && map[sum] && map[sum] == i) {
+      sol += sum + i;
+    }
+  }
+
+  console.log(sol);
+
+})();
